7e37fa0024c8634110be158f21153de73f0d8ea7
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2022-10-26 17:13:55 -0300

    projeto finalizado

---
+++ js/reconhecimentoDeVoz.js
@@ -0,0 +1,27 @@
+const elementoChute = document.getElementById('chute')
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela (chute)
+    verificaSeOChutePossuiUmValorValido(chute)
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+    `
+}
+
+// para o microfone não encerrar
+recognition.addEventListener('end', () => recognition.start())
